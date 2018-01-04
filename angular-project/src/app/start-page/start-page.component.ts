b31d99a759715b65fc3f68f0116e1da042df9ac1
Anna Wagner <annwagner8888@gmail.com>
2018-01-04 18:09:25 +0200

    Added button-help to navbar, show and hide cards of budglet with scroll.

--- angular-project/src/app/start-page/start-page.component.ts
+++ angular-project/src/app/start-page/start-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 
 @Component({
   selector: 'app-start-page',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-page.component.scss']
 })
 export class StartPageComponent implements OnInit {
+  isShowCards:boolean = false;
 
-  constructor() { }
+  constructor(private spinnerService: Ng4LoadingSpinnerService) { }
 
   ngOnInit() {
+    this.spinnerService.show();
+    this.spinnerService.show();
+    setTimeout(function () { this.spinnerService.hide() }.bind(this), 1500);
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    let position = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    let element = document.getElementById('startBudges');
+    let positTop = element.getBoundingClientRect().top;
+    let positBottom = element.getBoundingClientRect().bottom;
+    if (position >= positTop && position <= positBottom) {
+      this.isShowCards = true;
+    } 
+    // else
+    // // this.isShowCards = false;
   }
 
 }
